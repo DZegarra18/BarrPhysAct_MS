054a5d4e3014469bc6f33dd18fe45b70041f532f
DanielZegarra <danizegarra18@gmail.com>
2026-03-09 19:24:25 +0100

    R Kappa first 200

---
+++ R/Kappa_first_200.R
@@ -0,0 +1,21 @@
+library(dplyr)
+library(stringr)
+library(irr)
+
+#Filtrar y tidy csv
+articles_first_200 <- read.csv("articles_first_200.csv")
+
+decisions <- articles_first_200|>
+            select(notes)
+decisions <-  decisions |> 
+            mutate(
+              Mateo = str_match(notes, '"Mateo"=>"([^"]+)"')[,2],
+              Daniel = str_match(notes, '"Daniel"=>"([^"]+)"')[,2]
+            ) |> 
+            select(Mateo, Daniel)
+
+decisions$Mateo <- as.factor(decisions$Mateo)
+decisions$Daniel <- as.factor(decisions$Daniel)
+
+#calcular kappa
+kappa2(decisions[, c("Mateo", "Daniel")])
